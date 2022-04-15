57c390d1b00b3bc50449be09964cd2c772e78e32
Eva <rutheve.eva@gmail.com>
2022-04-15 19:15:19 +0300

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  devKey: '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'
 };
 
 /*
